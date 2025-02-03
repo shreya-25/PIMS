8048c7f3c603404b0c4cca617bba0e44470a8c6c
shreya-25 <shreyaagarwal252000@gmail.com>
2025-02-02 21:07:27 -0500

    only frontend in github-pges

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {register, login} = require("../controller/authController");
-
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-module.exports = router;
