b044f6607eaa07a2d10950a9881e32d39973073f
Shreya Agarwal <shreyaagarwal252000@gmail.com>
2025-02-11 14:05:32 -0500

    admin section

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -24,45 +24,45 @@ const Filter = () => {
       <div className="item">
         {/* Country Dropdown */}
         <select
-          name="country"
+          name="Case Number"
           value={filters.country}
           onChange={handleChange}
           className="filter-dropdown"
         >
-          <option value="">Country</option>
-          <option value="USA">USA</option>
-          <option value="UK">UK</option>
-          <option value="Germany">Germany</option>
+          <option value="">Case Number</option>
+          <option value="12345">12345</option>
+          <option value="45607">45607</option>
+          <option value="23789">23789</option>
         </select>
       </div>
 
       <div className="item">
         {/* Brand Dropdown */}
         <select
-          name="brand"
+          name="Case Name"
           value={filters.brand}
           onChange={handleChange}
           className="filter-dropdown"
         >
-          <option value="">Brand</option>
-          <option value="Heineken">Heineken</option>
-          <option value="Budweiser">Budweiser</option>
-          <option value="Corona">Corona</option>
+          <option value="">Case Name</option>
+          <option value="Main Street Murder">Main Street Murder</option>
+          <option value="Cook Street Stolen Truck">Cook Street Stolen Truck</option>
+          <option value="216 Endicott Burglary">216 Endicott Burglary</option>
         </select>
       </div>
 
       <div className="item">
         {/* ABV Dropdown */}
         <select
-          name="abv"
+          name="Case Manager"
           value={filters.abv}
           onChange={handleChange}
           className="filter-dropdown"
         >
-          <option value="">ABV</option>
-          <option value="5%">5%</option>
-          <option value="6%">6%</option>
-          <option value="7%">7%</option>
+          <option value="">Case Manager</option>
+          <option value="Officer 1">Officer 1</option>
+          <option value="Officer 2">Officer 2</option>
+          <option value="Officer 3">Officer 3</option>
         </select>
       </div>
 
