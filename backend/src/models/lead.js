1b143a943b2edf59b6a78f69caff041a7084672d
Shreya Agarwal <shreyaagarwal252000@gmail.com>
2025-02-18 21:10:43 -0500

    create lead workflow

--- backend/src/models/lead.js
+++ backend/src/models/lead.js
@@ -4,15 +4,23 @@ const leadSchema = new mongoose.Schema(
     {
         // leadNo: { type: Number, required: true, unique: true },
         leadNo: { type: Number, required: true },
-        parentLeadNo: { type: Number }, // Reference to parent lead
+        parentLeadNo:{
+            type: [Number], // an array of Numbers
+            default: [],    // (optional) defaults to an empty array
+          },
         incidentNo: { type: String },
-        subNumber: { type: Number },
-        // associatedSubNumbers: [{ type: Number }], // List of sub-numbers
+        subNumber: { type: String },
+        associatedSubNumbers: [{ type: String}],
         assignedDate: { type: Date, required: true },
         assignedTo: [{ type: String }], // Array of officer names
         assignedBy: { type: String, required: true },
         summary: { type: String, required: true },
         description: { type: String, required: true },
+        leadStatus: { type: String, required: true },
+        dueDate: { type: Date },
+        priority:  { type: String },
+        caseName: { type: String, required: true },
+        caseNo: { type: String, required: true },
     },
     { timestamps: true }
 );
