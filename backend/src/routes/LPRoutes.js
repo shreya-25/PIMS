cc0fc188c54c18d1bbe1e1111613688ef5657578
Shreya Agarwal <shreyaagarwal252000@gmail.com>
2025-03-06 19:03:47 -0500

    lead desk

--- backend/src/routes/LPRoutes.js
+++ backend/src/routes/LPRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createLRPerson, getLRPersonByDetails } = require("../controller/LRPersonController");
+const { createLRPerson, getLRPersonByDetails, getLRPersonByDetailsandid } = require("../controller/LRPersonController");
 const verifyToken = require("../middleware/authMiddleware");
 const { roleMiddleware } = require("../middleware/roleMiddleware");
 
@@ -7,5 +7,8 @@ const router = express.Router();
 router.post("/lrperson", verifyToken, createLRPerson);
 
 router.get("/lrperson/:leadNo/:leadName/:caseNo/:caseName", verifyToken, getLRPersonByDetails);
+
+router.get("/lrperson/:leadNo/:leadName/:caseNo/:caseName/:id", verifyToken, getLRPersonByDetailsandid);
+
 
 module.exports = router;
