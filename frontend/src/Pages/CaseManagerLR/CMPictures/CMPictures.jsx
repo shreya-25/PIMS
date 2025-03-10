f62a04339efcf4dcbaafb0d3b75ea3460917dc67
Shreya Agarwal <shreyaagarwal252000@gmail.com>
2025-03-10 14:58:09 -0400

    fixes

--- frontend/src/Pages/CaseManagerLR/CMPictures/CMPictures.jsx
+++ frontend/src/Pages/CaseManagerLR/CMPictures/CMPictures.jsx
@@ -190,7 +190,24 @@ export const CMPictures = () => {
             ))}
           </tbody>
         </table>
+        <div className = "content-to-add">
+     
+     <h4 className="return-form-h4"> Add Comment</h4>
+       <div className="return-form">
+         <textarea
+          //  value={returnData.results}
+          //  onChange={(e) => handleInputChange("results", e.target.value)}
+           placeholder="Enter comments"
+         ></textarea>
+       </div>
 
+       <div className="form-buttons-return">
+         <button className="save-btn1">Add Comment</button>
+         {/* <button className="back-btn" onClick={() => handleNavigation("/LRPerson")}>Back</button>
+         <button className="next-btn" onClick={() => handleNavigation("/LRScratchpad")}>Next</button>
+         <button className="cancel-btn" onClick={() => setReturnData({ results: "" })}>Cancel</button> */}
+       </div>
+</div>
       
       </div>
       <FootBar
